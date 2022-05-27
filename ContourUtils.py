e352ad5e2b0dfa94da07f3ba606eefd19070ed48
Lars Gudjons <lars.gudjons@sit.fraunhofer.de>
2022-05-27 21:40:45 +0200

    Refactoring in main. External function for outer ellipse detection and limit setting. Added possibility to quit with q. Recorded a performance_profile_1.png

--- ContourUtils.py
+++ ContourUtils.py
@@ -143,78 +143,7 @@ def extract_roi_from_4_aruco_markers(frame, dsize=(500, 500), draw=False, use_ou
             return warped_image2
 
 
-def intersectLines(pt1, pt2, ptA, ptB):
-    """ this returns the intersection of Line(pt1,pt2) and Line(ptA,ptB)
 
-        returns a tuple: (xi, yi, valid, r, s), where
-        (xi, yi) is the intersection
-        r is the scalar multiple such that (xi,yi) = pt1 + r*(pt2-pt1)
-        s is the scalar multiple such that (xi,yi) = pt1 + s*(ptB-ptA)
-            valid == 0 if there are 0 or inf. intersections (invalid)
-            valid == 1 if it has a unique intersection ON the segment    """
-
-    DET_TOLERANCE = 0.00000001
-
-    # the first line is pt1 + r*(pt2-pt1)
-    # in component form:
-    x1, y1 = pt1
-    x2, y2 = pt2
-    dx1 = x2 - x1
-    dy1 = y2 - y1
-
-    # the second line is ptA + s*(ptB-ptA)
-    x, y = ptA
-    xB, yB = ptB
-    dx = xB - x
-    dy = yB - y
-
-    DET = (-dx1 * dy + dy1 * dx)
-
-    if math.fabs(DET) < DET_TOLERANCE:
-        return 0, 0
-
-    # now, the determinant should be OK
-    DETinv = 1.0 / DET
-
-    # find the scalar amount along the "self" segment
-    r = DETinv * (-dy * (x - x1) + dx * (y - y1))
-
-    # find the scalar amount along the input line
-    s = DETinv * (-dy1 * (x - x1) + dx1 * (y - y1))
-
-    # return the average of the two descriptions
-    x = (x1 + r * dx1 + x + s * dx) / 2.0
-    y = (y1 + r * dy1 + y + s * dy) / 2.0
-    return x, y
-
-
-def intersectLineCircle(center, radius, p1, p2):
-    baX = p2[0] - p1[0]
-    baY = p2[1] - p1[1]
-    caX = center[0] - p1[0]
-    caY = center[1] - p1[1]
-
-    a = baX * baX + baY * baY
-    bBy2 = baX * caX + baY * caY
-    c = caX * caX + caY * caY - radius * radius
-
-    pBy2 = bBy2 / a
-    q = c / a
-
-    disc = pBy2 * pBy2 - q
-    if disc < 0:
-        return False, None, False, None
-
-    tmpSqrt = math.sqrt(disc)
-    abScalingFactor1 = -pBy2 + tmpSqrt
-    abScalingFactor2 = -pBy2 - tmpSqrt
-
-    pint1 = p1[0] - baX * abScalingFactor1, p1[1] - baY * abScalingFactor1
-    if disc == 0:
-        return True, pint1, False, None
-
-    pint2 = p1[0] - baX * abScalingFactor2, p1[1] - baY * abScalingFactor2
-    return True, pint1, True, pint2
 
 
 
--- main.py
+++ main.py
@@ -34,13 +34,12 @@ rows = 6  # 17   6
 columns = 9  # 28    9
 squareSize = 30  # mm
 calibrationRuns = 1
-CAMERA_NUMBER = 0   # 0,1 is built-in, 2 is external webcam
+CAMERA_NUMBER = 0  # 0,1 is built-in, 2 is external webcam
 TRIANGLE_DETECT_THRESH = 24
 useMovingAverage = False
 
 # OpenCV Window GUI###############################
 gui.create_gui()
-
 
 # #################  Program Starting Screen  ########################
 keyEvent = cv2.waitKey(0)  # next image
@@ -129,6 +128,39 @@ while True:
 
 images_for_rolling_average = []
 ellipse = None
+x_offset_current, y_offset_current = 0, 0
+
+
+def detect_dart_circle_and_set_limits():
+    global contours, radius_1, radius_2, radius_3, radius_4, radius_5, radius_6, cnt, ellipse, x, y, a, b, angle, center_ellipse, x_offset_current, y_offset_current
+    imgContours, contours, imgCanny = ContourUtils.get_contours(img=img_roi, cThr=(cannyLow, cannyHigh),
+                                                                gaussFilters=noGauss, minArea=minArea,
+                                                                epsilon=epsilon, draw=False,
+                                                                erosions=erosions, dilations=dilations,
+                                                                showFilters=showFilters)
+    radius_1, radius_2, radius_3, radius_4, radius_5, radius_6, x_offset, y_offset = gui.update_dart_trackbars()
+    # Create Radien in pixels
+    for cnt in contours:
+        if 200000 / 4 < cnt[1] < 1000000 / 4:
+            # Create the outermost Circle
+            if ellipse is None or x_offset_current != x_offset or y_offset_current != y_offset:  # Save the outer most ellipse for later to avoid useless re calculation !
+                x_offset_current, y_offset_current = x_offset, y_offset
+                ellipse = cv2.fitEllipse(cnt[4])  # Also a benefit for stability of the outer ellipse --> not jumping from frame to frame
+                x, y = ellipse[0]
+                a, b = ellipse[1]
+                angle = ellipse[2]
+                center_ellipse = (int(x + x_offset / 10), int(y + y_offset / 10))
+                a = a / 2
+                b = b / 2
+                # set the limits also only once
+                dart_scorer_util.bullsLimit = a * (radius_1 / 100)
+                dart_scorer_util.singleBullsLimit = a * (radius_2 / 100)
+                dart_scorer_util.innerTripleLimit = a * (radius_3 / 100)
+                dart_scorer_util.outerTripleLimit = a * (radius_4 / 100)
+                dart_scorer_util.innerDoubleLimit = a * (radius_5 / 100)
+                dart_scorer_util.outerBoardLimit = a * (radius_6 / 100)
+
+
 while True:
     fpsReader = FPS()
     success, img = cap.read()
@@ -141,132 +173,107 @@ while True:
 
             cannyLow, cannyHigh, noGauss, minArea, erosions, dilations, epsilon, showFilters, automaticMode, threshold_new = gui.updateTrackBar()
 
-            # get Contours from Image
-            imgContours, contours, imgCanny = ContourUtils.get_contours(img=img_roi, cThr=(cannyLow, cannyHigh),
-                                                                        gaussFilters=noGauss, minArea=minArea,
-                                                                        epsilon=epsilon, draw=False,
-                                                                        erosions=erosions, dilations=dilations,
-                                                                        showFilters=showFilters)
+            detect_dart_circle_and_set_limits()
 
-            radius_1, radius_2, radius_3, radius_4, radius_5, radius_6, x_offset, y_offset = gui.update_dart_trackbars()
-            # Create Radien in pixels
+            # get the difference image
+            difference = cv2.absdiff(img_roi, default_img)
+            # blur it for better edges
+            blur = cv2.GaussianBlur(difference, (5, 5), 0)
+            for i in range(10):
+                blur = cv2.GaussianBlur(blur, (9, 9), 1)
+            blur = cv2.bilateralFilter(blur, 9, 75, 75)
+            ret, thresh = cv2.threshold(blur, TRIANGLE_DETECT_THRESH, 255, 0)
 
-            for cnt in contours:
-                if 200000 / 4 < cnt[1] < 1000000 / 4:
-                    # Create the outermost Circle
-                    if ellipse is None:     # Save the outer most ellipse for later to avoid useless re calculation !
-                        ellipse = cv2.fitEllipse(cnt[4])    # Also a benefit for stability of the outer ellipse --> not jumping from frame to frame
-                        x, y = ellipse[0]
-                        a, b = ellipse[1]
-                        angle = ellipse[2]
-                        center_ellipse = (int(x + x_offset / 10), int(y + y_offset / 10))
-                        a = a / 2
-                        b = b / 2
+            if useMovingAverage:
+                images_for_rolling_average.append(thresh)
+                nr_imgs = 10
+                if len(images_for_rolling_average) > nr_imgs:
+                    out = images_for_rolling_average[-1]
+                    for j in range(nr_imgs - 1, 0, -1):
+                        # out = cv2.add(images_for_rolling_average[j], out)
+                        out = cv2.addWeighted(images_for_rolling_average[j], 0.5, out, 0.5, 1)
+                    images_for_rolling_average = images_for_rolling_average[1:20]
+                else:
+                    out = thresh
+            else:
+                out = thresh
 
-                    dart_scorer_util.bullsLimit = a * (radius_1 / 100)
-                    dart_scorer_util.singleBullsLimit = a * (radius_2 / 100)
-                    dart_scorer_util.innerTripleLimit = a * (radius_3 / 100)
-                    dart_scorer_util.outerTripleLimit = a * (radius_4 / 100)
-                    dart_scorer_util.innerDoubleLimit = a * (radius_5 / 100)
-                    dart_scorer_util.outerBoardLimit = a * (radius_6 / 100)
+            gray = cv2.cvtColor(out, cv2.COLOR_BGR2GRAY)
+            cv2.imshow("GRAY", gray)
+            contours, hierarchy = cv2.findContours(gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            minArea = 800
 
-                    # get the difference image
-                    difference = cv2.absdiff(img_roi, default_img)
-                    # blur it for better edges
-                    blur = cv2.GaussianBlur(difference, (5, 5), 0)
+            for i in contours:
+                area = cv2.contourArea(i)
+                if area > minArea:
+                    points_list = i.reshape(i.shape[0], i.shape[2])
+                    triangle = cv2.minEnclosingTriangle(cv2.UMat(points_list.astype(np.float32)))
+                    triangle_np_array = cv2.UMat.get(triangle[1])
+                    pt1, pt2, pt3 = triangle_np_array.astype(np.int32)
 
-                    for i in range(10):
-                        blur = cv2.GaussianBlur(blur, (9, 9), 1)
-                    blur = cv2.bilateralFilter(blur, 9, 75, 75)
-                    ret, thresh = cv2.threshold(blur, TRIANGLE_DETECT_THRESH, 255, 0)
+                    # Display the triangles
+                    cv2.line(thresh, pt1.ravel(), pt2.ravel(), (255, 0, 255), 2)
+                    cv2.line(thresh, pt2.ravel(), pt3.ravel(), (255, 0, 255), 2)
+                    cv2.line(thresh, pt3.ravel(), pt1.ravel(), (255, 0, 255), 2)
 
-                    if useMovingAverage:
-                        images_for_rolling_average.append(thresh)
-                        nr_imgs = 10
-                        if len(images_for_rolling_average) > nr_imgs:
-                            out = images_for_rolling_average[-1]
-                            for j in range(nr_imgs-1, 0, -1):
-                                # out = cv2.add(images_for_rolling_average[j], out)
-                                out = cv2.addWeighted(images_for_rolling_average[j], 0.5, out, 0.5, 1)
-                            images_for_rolling_average = images_for_rolling_average[1:20]
-                        else:
-                            out = thresh
-                    else:
-                        out = thresh
+                    dart_point, rest_pts = dart_scorer_util.findTipOfDart(pt1, pt2, pt3)
 
-                    gray = cv2.cvtColor(out, cv2.COLOR_BGR2GRAY)
-                    cv2.imshow("GRAY", gray)
-                    contours, hierarchy = cv2.findContours(gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-                    minArea = 800
+                    # Display the Dart point
+                    cv2.circle(thresh, dart_point, 4, (0, 0, 255), -1)
+                    cv2.circle(img_roi, dart_point, 4, (0, 0, 255), -1)
 
-                    for i in contours:
-                        area = cv2.contourArea(i)
-                        if area > minArea:
-                            points_list = i.reshape(i.shape[0], i.shape[2])
-                            triangle = cv2.minEnclosingTriangle(cv2.UMat(points_list.astype(np.float32)))
-                            triangle_np_array = cv2.UMat.get(triangle[1])
-                            pt1, pt2, pt3 = triangle_np_array.astype(np.int32)
+                    radius, angle = dart_scorer_util.getRadiusAndAngle(center_ellipse[0], center_ellipse[1], dart_point[0], dart_point[1])
+                    value, mult = dart_scorer_util.evaluateThrow(radius, angle)
+                    print("First guess: ", value, mult)
 
-                            # Display the triangles
-                            cv2.line(thresh, pt1.ravel(), pt2.ravel(), (255, 0, 255), 2)
-                            cv2.line(thresh, pt2.ravel(), pt3.ravel(), (255, 0, 255), 2)
-                            cv2.line(thresh, pt3.ravel(), pt1.ravel(), (255, 0, 255), 2)
+                    bottom_point = dart_scorer_util.getBottomPoint(rest_pts[0], rest_pts[1], dart_point)
+                    cv2.line(img_roi, dart_point, bottom_point, (0, 0, 255), 2)
 
-                            dart_point, rest_pts = dart_scorer_util.findTipOfDart(pt1, pt2, pt3)
+                    k = -0.25  # scaling factor
+                    vect = (dart_point - bottom_point)
+                    new_dart_point = dart_point + k * vect
+                    cv2.circle(img_roi, new_dart_point.astype(np.int32), 4, (0, 255, 0), -1)
+                    new_radius, new_angle = dart_scorer_util.getRadiusAndAngle(center_ellipse[0], center_ellipse[1], new_dart_point[0], new_dart_point[1])
+                    new_val, new_mult = dart_scorer_util.evaluateThrow(new_radius, new_angle)
+                    print("Second guess: ", new_val, new_mult)
 
-                            # Display the Dart point
-                            cv2.circle(thresh, dart_point, 4, (0, 0, 255), -1)
-                            cv2.circle(img_roi, dart_point, 4, (0, 0, 255), -1)
+            cv2.imshow("Threshold", out)
+            x = 3000
+            # if cv2.countNonZero(thresh) > x and cv2.countNonZero(thresh) < 15000:  # threshold important -> make accessible
 
-                            radius, angle = dart_scorer_util.getRadiusAndAngle(center_ellipse[0], center_ellipse[1], dart_point[0], dart_point[1])
-                            value, mult = dart_scorer_util.evaluateThrow(radius, angle)
-                            print("First guess: ", value, mult)
+            previous_img = img_roi
+            # TODO: Separate show image and processing image with cv2.copy
+            cv2.ellipse(img_roi, (int(x), int(y)), (int(a), int(b)), int(angle), 0.0, 360.0, (255, 0, 0))
+            cv2.circle(img_roi, center_ellipse, int(a * (radius_1 / 100)), (255, 0, 255), 1)
+            cv2.circle(img_roi, center_ellipse, int(a * (radius_2 / 100)), (255, 0, 255), 1)
+            cv2.circle(img_roi, center_ellipse, int(a * (radius_3 / 100)), (255, 0, 255), 1)
+            cv2.circle(img_roi, center_ellipse, int(a * (radius_4 / 100)), (255, 0, 255), 1)
+            cv2.circle(img_roi, center_ellipse, int(a * (radius_5 / 100)), (255, 0, 255), 1)
+            cv2.circle(img_roi, center_ellipse, int(a * (radius_6 / 100)), (255, 0, 255), 1)
 
-                            bottom_point = dart_scorer_util.getBottomPoint(rest_pts[0], rest_pts[1], dart_point)
-                            cv2.line(img_roi, dart_point, bottom_point, (0, 0, 255), 2)
+            cv2.ellipse(img_roi, ellipse, (0, 255, 0), 5)
+            if dart_point is not None:
+                cv2.line(img_roi, center_ellipse, dart_point, (255, 0, 0), 2)
 
-                            k = -0.25  # scaling factor
-                            vect = (dart_point - bottom_point)
-                            new_dart_point = dart_point + k * vect
-                            cv2.circle(img_roi, new_dart_point.astype(np.int32), 4, (0, 255, 0), -1)
-                            new_radius, new_angle = dart_scorer_util.getRadiusAndAngle(center_ellipse[0], center_ellipse[1], new_dart_point[0], new_dart_point[1])
-                            new_val, new_mult = dart_scorer_util.evaluateThrow(new_radius, new_angle)
-                            print("Second guess: ", new_val, new_mult)
+            # vertex calculation
+            xb = b * math.cos(angle)
+            yb = b * math.sin(angle)
 
-                    cv2.imshow("Threshold", out)
-                    x = 3000
-                    # if cv2.countNonZero(thresh) > x and cv2.countNonZero(thresh) < 15000:  # threshold important -> make accessible
+            xa = a * math.sin(angle)
+            ya = a * math.cos(angle)
 
-                    previous_img = img_roi
-                    # TODO: Separate show image and processing image with cv2.copy
-                    cv2.ellipse(img_roi, (int(x), int(y)), (int(a), int(b)), int(angle), 0.0, 360.0, (255, 0, 0))
-                    cv2.circle(img_roi, center_ellipse, int(a * (radius_1 / 100)), (255, 0, 255), 1)
-                    cv2.circle(img_roi, center_ellipse, int(a * (radius_2 / 100)), (255, 0, 255), 1)
-                    cv2.circle(img_roi, center_ellipse, int(a * (radius_3 / 100)), (255, 0, 255), 1)
-                    cv2.circle(img_roi, center_ellipse, int(a * (radius_4 / 100)), (255, 0, 255), 1)
-                    cv2.circle(img_roi, center_ellipse, int(a * (radius_5 / 100)), (255, 0, 255), 1)
-                    cv2.circle(img_roi, center_ellipse, int(a * (radius_6 / 100)), (255, 0, 255), 1)
-
-                    cv2.ellipse(img_roi, ellipse, (0, 255, 0), 5)
-                    if dart_point is not None:
-                        cv2.line(img_roi, center_ellipse, dart_point, (255, 0, 0), 2)
-
-                    # vertex calculation
-                    xb = b * math.cos(angle)
-                    yb = b * math.sin(angle)
-
-                    xa = a * math.sin(angle)
-                    ya = a * math.cos(angle)
-
-                    rect = cv2.minAreaRect(cnt[4])
-                    box = cv2.boxPoints(rect)
-                    box = np.int0(box)
-                    # cv2.drawContours(img_undist, [box], 0, (0, 0, 255), 2)
+            # rect = cv2.minAreaRect(cnt[4])
+            # box = cv2.boxPoints(rect)
+            # box = np.int0(box)
+            # cv2.drawContours(img_undist, [box], 0, (0, 0, 255), 2)
             fps, img_roi = fpsReader.update(img_roi)
-            cv2.imshow("Dart Settings", rez(img_roi, 2))
+            cv2.imshow("Dart Settings", img_roi)
         else:
             print("NO MARKERS FOUND!")
             cv2.putText(img_undist, "NO MARKERS FOUND", (300, 300), cv2.FONT_HERSHEY_COMPLEX, 3, (255, 0, 255), 3)
             cv2.imshow("Dart Settings", img_undist)
 
         cv2.waitKey(1)
+        if cv2.waitKey(1) & 0xff == ord('q'):
+            cap.release()
+            exit()
